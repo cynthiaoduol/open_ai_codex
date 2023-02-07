f44f5e5593b32af830d6404808dd283e520edbb3
cynthiaoduol <cynthiaobu940@gmail.com>
2023-02-07 12:17:09 +0300

    create a stripe for each message

--- client/main.js
+++ client/main.js
@@ -46,4 +46,18 @@ const generateUniqueId=()=>{
 }
 
 
-
+const chatStripe=(isAi, value, uniqueId)=> {
+  return `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
+}
