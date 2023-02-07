81d3c88f4b0374305c29f517b1cc99a0861a384e
cynthiaoduol <cynthiaobu940@gmail.com>
2023-02-07 12:08:52 +0300

    add a typing functionality

--- client/main.js
+++ client/main.js
@@ -21,3 +21,17 @@ const loader = (element) => {
   }, 300);
 };
 
+const typeText = (element, text) => {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+};
+
+
